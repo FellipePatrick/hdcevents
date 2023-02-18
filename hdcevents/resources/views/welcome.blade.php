0bc127f6fd358314e94ae5363e5e698ea1cafea1
pokytester <fellipe.patrick678@gmail.com>
2023-02-18 15:08:28 -0300

    adicionando projeto

---
+++ hdcevents/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('title', 'HDC Events')
+
+@section('content')
+
+<h1>Algum título</h1>
+<img src="/img/banner.jpg" alt="Banner" class="w-100">
+
+<!-- Comentário do HTML -->
+{{-- Este é o comentário do Blade --}}
+
+@endsection
